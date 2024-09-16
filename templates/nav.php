751c52e8adf7be1464c876eca90751560df44d9f
Syndrom2211 <yukmari2211@gmail.com>
2024-09-16 09:06:39 +0700

    Nambahin menu di Program Pelajar

--- templates/nav.php
+++ templates/nav.php
@@ -48,6 +48,7 @@
                                         <li class="nav-item"><a href="konsul-akademik" class="nav-link">Konsultasi Tugas Akademik</a></li>
                                         <li class="nav-item"><a href="virtual-classroom" class="nav-link">Virtual Classroom</a></li>
                                         <li class="nav-item"><a href="https://www.instagram.com/p/C_wz3xHydeA/" class="nav-link" target="_blank">Komunitas Siber YMP</a></li>
+                                        <li class="nav-item"><a href="https://www.instagram.com/p/C_9VjstTrFT/" class="nav-link" target="_blank">Bimbel MAFIKI</a></li>
                                     </ul>
                                 </li>
                                 <li class="nav-item"><a href="contact" class="nav-link">Kontak Kami</a></li>
