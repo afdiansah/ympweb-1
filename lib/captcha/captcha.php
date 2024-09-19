fda3960b08ad1a69cbe31286c3b9510042a3c9a6
Syndrom2211 <yukmari2211@gmail.com>
2024-09-19 15:04:58 +0700

    Perbaikan Kirim Email Status Pembayaran

--- lib/captcha/captcha.php
+++ lib/captcha/captcha.php
@@ -27,7 +27,7 @@ $_SESSION['captcha'] = $captcha_text;
 // Local
 //$font_path = $_SERVER['DOCUMENT_ROOT'] . '/ympweb/lib/captcha/font.ttf';
 // Online Hosting
-$font_path = $_SERVER['DOCUMENT_ROOT'] . '/lib/captcha/font.ttf';//
+$font_path = $_SERVER['DOCUMENT_ROOT'] . '/lib/captcha/font.ttf';
 
 imagettftext($image, 20, 0, 10, 30, $text_color, $font_path, $captcha_text);
 
