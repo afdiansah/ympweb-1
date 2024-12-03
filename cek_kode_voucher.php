e5745b90e33380adbddf0d489da9a7862291693f
Syndrom2211 <yukmari2211@gmail.com>
2024-12-03 20:12:59 +0700

    perubahan kode voucher

--- cek_kode_voucher.php
+++ cek_kode_voucher.php
@@ -3,7 +3,8 @@ header('Content-Type: application/json');
 
 // Simulasi data kode voucher
 $available_vouchers = [
-    'ymp_bersatu' => 0.75, // Diskon 75%
+    //'ymp_bersatu' => 0.75, // Diskon 75%
+    'C0fnR!23C9W*ox' => 0.75, // Diskon 75%
 ];
 
 // Data daftar kelas dan harga valid
